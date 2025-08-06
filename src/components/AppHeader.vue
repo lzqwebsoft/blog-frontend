db1c0abe30caa2fdc1379fd22491496c0d4a14e3
zqluo <zqluo@china-qizhi.com>
2025-08-06 16:56:03 +0800

    添加font awesome图标支持

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,11 +9,20 @@
                 <div class="nav-menu">
                     <input type="text" class="search-input" placeholder="搜索" />
 
+                    <button class="theme-toggle" @click="toggleTheme" :title="isDark ? '切换到浅色模式' : '切换到深色模式'">
+                        <font-awesome-icon :icon="isDark ? 'sun' : 'moon'" />
+                    </button>
+
                     <div class="dropdown">
-                        <button class="nav-item" @click="showMenu = !showMenu">☰</button>
+                        <button class="nav-item" @click="showMenu = !showMenu">
+                            <font-awesome-icon icon="bars" class="menu" />
+                        </button>
                         <div class="dropdown-content" v-show="showMenu">
                             <RouterLink v-for="(item, index) in menuItems" :key="index" :to="item.url"
-                                class="dropdown-item" v-html="item.title" />
+                                class="dropdown-item">
+                                <font-awesome-icon :icon="item.icon" class="menu-icon" />
+                                <span v-html="item.title"></span>
+                            </RouterLink>
                         </div>
                     </div>
                 </div>
@@ -31,13 +40,21 @@ export default {
             isHidden: false,
             previousTop: 0,
             scrolling: false,
+            isDark: false,
             menuItems: [
-                { title: '发表博客', url: '/article/new.html' },
-                { title: '修改密码', url: '/changepwd.html' },
-                { title: '设&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;置', url: '/set.html' },
-                { title: '注&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;销', url: '/singleout.html' },
+                { title: '发表博客', url: '/article/new.html', icon: 'edit' },
+                { title: '修改密码', url: '/changepwd.html', icon: 'lock' },
+                { title: '设&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;置', url: '/set.html', icon: 'cog' },
+                { title: '注&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;销', url: '/singleout.html', icon: 'sign-out-alt' },
             ],
         }
+    },
+    mounted() {
+        window.addEventListener('scroll', this.handleScroll)
+        this.initializeTheme()
+    },
+    unmounted() {
+        window.removeEventListener('scroll', this.handleScroll)
     },
     methods: {
         handleScroll() {
@@ -64,12 +81,25 @@ export default {
             this.previousTop = currentTop
             this.scrolling = false
         },
-    },
-    mounted() {
-        window.addEventListener('scroll', this.handleScroll)
-    },
-    unmounted() {
-        window.removeEventListener('scroll', this.handleScroll)
+        toggleTheme() {
+            this.isDark = !this.isDark
+            this.applyTheme()
+            localStorage.setItem('theme', this.isDark ? 'dark' : 'light')
+        },
+        initializeTheme() {
+            const savedTheme = localStorage.getItem('theme')
+            const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+            this.isDark = savedTheme ? savedTheme === 'dark' : prefersDark
+            this.applyTheme()
+        },
+        applyTheme() {
+            if (this.isDark) {
+                document.documentElement.classList.add('dark-theme')
+            } else {
+                document.documentElement.classList.remove('dark-theme')
+            }
+        },
     },
 }
 </script>
@@ -149,6 +179,35 @@ export default {
 
 .dropdown-item:last-child {
     border-bottom: none;
+}
+
+.menu-icon {
+    margin-right: 0.5rem;
+    width: 16px;
+    text-align: center;
+}
+
+.theme-toggle {
+    background: none;
+    border: none;
+    cursor: pointer;
+    font-size: 1.2rem;
+    color: #333;
+    padding: 0.5rem;
+    border-radius: 4px;
+    transition: background-color 0.3s ease;
+}
+
+.theme-toggle:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+}
+
+:root.dark-theme .theme-toggle {
+    color: #fff;
+}
+
+:root.dark-theme .theme-toggle:hover {
+    background-color: rgba(255, 255, 255, 0.1);
 }
 
 @media (max-width: 768px) {
--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import './assets/styles/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEdit, faLock, faCog, faSignOutAlt, faMoon, faSun, faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import App from './App.vue'
 import router from './router'
+
+library.add(faEdit, faLock, faCog, faSignOutAlt, faMoon, faSun, faBars)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
