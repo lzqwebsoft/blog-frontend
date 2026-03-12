84bb8452915698b340adfdc39e51d19ad24b3925
lzqwebsoft <lzqwebsoft@gmail.com>
2026-03-12 11:30:07 +0800

    添加备案号

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,8 +6,15 @@
                 <a href="https://github.com/lzqwebsoft" target="_blank">-ZQLUO-</a>.
             </p>
             <p>
-                <a href="https://beian.miit.gov.cn/" target="_blank">沪ICP备2025128934号-2</a> &nbsp;&nbsp;
                 Copyright &copy;2012 - {{ currentYear }}
+            </p>
+            <p class="filing">
+                <a href="https://beian.mps.gov.cn/#/query/webSearch?code=31011202022211" rel="noreferrer"
+                    target="_blank">
+                    <img src="@/assets/images/filing.png" alt="备案图标" />
+                    <span>沪公网安备31011202022211号</span>
+                </a>
+                <a href="https://beian.miit.gov.cn/" target="_blank"><span>沪ICP备2025128934号-2</span></a>
             </p>
         </div>
     </footer>
@@ -37,5 +44,36 @@ export default {
 
 .footer a {
     color: #007bff;
+    text-decoration: none;
+}
+
+.filing {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    margin-top: 0.5rem;
+}
+
+.filing a {
+    display: inline-flex;
+    align-items: center;
+    color: var(--text-color);
+    padding: 0.2rem 0.5rem;
+}
+
+.filing a:hover {
+    color: #007bff;
+    text-decoration: underline;
+}
+
+.filing a:first-child {
+    margin-right: 10px;
+}
+
+.filing img {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
 }
 </style>
